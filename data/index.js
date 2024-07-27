5e7175622e4d1487e9b950a88328eec4d40d4765
Utsav Kumar <utsav.kumar@highspot.com>
2024-07-27 15:01:10 -0700

    Added dummy data for file explorer

---
+++ data/index.js
@@ -0,0 +1,53 @@
+// Description: Data for the file explorer
+
+import { documents } from "./documents";
+import { images } from "./images";
+
+export const TYPES = {
+  FOLDER: 'folder',
+  FILE: 'file'
+};
+
+const data = [
+  {
+    type: TYPES.FOLDER,
+    name: 'Files',
+    modified: new Date('2020-07-06'),
+    size: 0,
+    children: [
+      {
+        type: TYPES.FOLDER,
+        name: 'Documents',
+        modified: new Date('2020-07-06'),
+        size: 0,
+        children: documents,
+      },
+      {
+        type: TYPES.FOLDER,
+        name: 'Images',
+        modified: new Date('2020-07-06'),
+        size: 0,
+        children: images,
+      },
+      {
+        type: TYPES.FOLDER,
+        name: 'System',
+        modified: new Date('2020-07-06'),
+        size: 0,
+        children: [],
+      },
+      {
+        type: TYPES.FILE,
+        name: 'Description.rtf',
+        modified: new Date('2020-07-06'),
+        size: 1024,
+      },
+      {
+        type: TYPES.FILE,
+        name: 'Description.txt',
+        modified: new Date('2020-07-06'),
+        size: 2048,
+      }
+    ]
+  }
+];
